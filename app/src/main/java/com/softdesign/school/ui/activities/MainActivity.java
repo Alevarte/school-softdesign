036fbf48b7ddc49c634f16c38281f6cd7265d174
Alevarte <alevartee@gmail.com>
2016-02-10 00:56:56 +0400

    Initial commit

---
+++ app/src/main/java/com/softdesign/school/ui/activities/MainActivity.java
@@ -0,0 +1,104 @@
+package com.softdesign.school.ui.activities;
+
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.softdesign.school.R;
+import com.softdesign.school.ui.fragments.ContactsFragment;
+import com.softdesign.school.ui.fragments.ProfileFragment;
+import com.softdesign.school.ui.fragments.SettingFragment;
+import com.softdesign.school.ui.fragments.TasksFragment;
+import com.softdesign.school.ui.fragments.TeamFragment;
+import com.softdesign.school.utils.Lg;
+
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar mActionBarToolbar;
+    NavigationView mNavigationView;
+    DrawerLayout mDrawerLayout;
+    Fragment mFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Lg.e(this.getLocalClassName(), "onCreate");
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setupToolbar();
+        setupDrawer();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_conteiner, new ProfileFragment()).commit();
+        }
+    }
+
+
+    /**
+     * Инициализирует тулбар
+     */
+    private void setupToolbar() {
+        setSupportActionBar(mActionBarToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    /**
+     * Создаем обработчик выбора пунктов меню
+     */
+    private void setupDrawer() {
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.drawer_profile:
+                        mFragment = new ProfileFragment();
+                        break;
+                    case R.id.drawer_contacts:
+                        mFragment = new ContactsFragment();
+                        break;
+                    case R.id.drawer_tasks:
+                        mFragment = new TasksFragment();
+                        break;
+                    case R.id.drawer_team:
+                        mFragment = new TeamFragment();
+                        break;
+                    case R.id.drawer_setting:
+                        mFragment = new SettingFragment();
+                        break;
+                }
+                if (mFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_conteiner, mFragment).addToBackStack(null).commit();
+                }
+                mDrawerLayout.closeDrawers();
+                return false;
+            }
+        });
+    }
+
+    /**
+     * Метод вызываемый в фрагментах для выделения пункта меню
+     */
+    public void setCheck(int id) {
+        mNavigationView.setCheckedItem(id);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
